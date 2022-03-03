6878b3239a6893e871975327869af361acf07efd
theertha <theerthakunjusss@gmail.com>
2022-03-03 16:04:51 +0530

    ds

---
+++ sorting.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void main()
+{
+int i, j, a, n, num[30];
+printf("Enter the value of N \n");
+scanf("%d", &n);
+printf("Enter the numbers \n");
+for (i = 0; i < n;i++)
+{
+scanf("%d", &num[i]);
+}
+for (i = 0;i<n-1;i++) 
+{
+for (j = i + 1; j < n;j++)
+{
+if (num[i] > num[j]) 
+{
+a =num[i];
+num[i] =num[j];
+num[j] = a;
+}
+}
+}
+printf("sorted elements are \n");
+for (i = 0; i < n;i++)
+{
+printf("%d\n", num[i]);
+}
+}
